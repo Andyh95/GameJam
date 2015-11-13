bbf12a079e641e8b58f7f76672c3ea64f8811b52
Keonaie <hkeonaie9462@gmail.com>
2015-11-13 23:23:14 +0000

    Added MenuScript with GameStart button press and sound toggle functionality, to be changed and uncommented on merge

---
+++ Assets/MenuScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuScript : MonoBehaviour {
+	public bool gameSoundOn = true;
+
+	public void OnClickPlay()
+	{
+		// This add the button click function to change from current scene to "GameScene"
+		//Application.LoadLevel ("Game"); // Uncomment previous code, change "Game to "name of the scene" and delete this comment after merging with Master branch to create connection to game scene.
+		print ("Scene Change!");
+	}
+
+	public void VolumeToggle()
+	{
+		// Add volume control code after merging to master branch
+		 gameSoundOn = !gameSoundOn;
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
