f2f7d606eede6b2992f431c38a56f017d848abea
Andrew <andy.hirst95@gmail.com>
2015-11-13 22:22:18 +0000

    Added scrolling background

---
+++ Assets/Scripts/ScrollingBackground.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScrollingBackground : MonoBehaviour {
+
+
+		
+	void Update()
+	{
+		MeshRenderer mr = GetComponent<MeshRenderer> ();
+			
+		Material mat = mr.material;
+			
+		Vector2 offset = mat.mainTextureOffset;
+			
+		offset.x += Time.deltaTime / 10f;
+			
+		mat.mainTextureOffset = offset;
+	}
+
+}
