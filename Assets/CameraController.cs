9d5d33cf5c827dd7298cb421ceaacbd41a131e04
Wilky <101wilky@gmail.com>
2015-11-13 23:54:26 +0000

    Camera is now set to internal camera for the mirror

--- Assets/CameraController.cs
+++ Assets/CameraController.cs
@@ -12,7 +12,24 @@ public class CameraController : MonoBehaviour
         Debug.Log("Script has been started");
         plane = GameObject.FindWithTag("Player");
 
-        mCamera = new WebCamTexture(2);
+  //      WebCamDevice[] devices = WebCamTexture.devices;
+  //      for (var i = 0; i < devices.Length; i++)
+  //          Debug.Log(devices[i].name);
+  //      }
+
+  //  WebCamDevice[] devices = WebCamTexture.devices;
+  
+  //foreach(WebcamDevice cam in devices)
+  //{
+  //   if(cam.isFrontFacing )
+  //   {    
+  //       webCameraTexture  =    new WebCamTexture(cameraIndex, Screen.width, Screen.height);
+  //  webCameraTexture.deviceName  = cam.name;
+  //       webCameraTexture.Play();
+  //   }
+  //}
+
+    mCamera = new WebCamTexture("Camera 1");
         plane.GetComponent<Renderer>().material.mainTexture = mCamera;
         mCamera.Play();
 
