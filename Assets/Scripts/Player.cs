f93f266bb9310f8d4bd9b42e6200669562c06f8b
Andrew <andy.hirst95@gmail.com>
2015-11-14 12:15:29 +0000

    fixed

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,47 +4,42 @@ using System.Collections;
 public class Player : MonoBehaviour {
 
 	public Animator anim;
+	public GameObject cat;
+
 	public GameObject particles;
 	bool falling = false;
-
-	public GameObject cat;
 	// Use this for initialization
 	void Start () {
 
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-
-
-
-		if (Input.GetButton("Fire1")) {
+		SoundScript sound = FindObjectOfType<SoundScript> ();
+		if (Input.GetKeyUp (KeyCode.Space)) {
 			anim.SetTrigger("Jump");
 			//transform.position += new Vector3(0,2.0f,0);
 			GetComponent<Rigidbody2D>().velocity = new Vector2 (0.0f,5.0f);
+			sound.IsJumpingSetter(true);
 			Debug.Log("Jump");
 			}
 		if (GetComponent<Rigidbody2D> ().velocity.y > 0) {
 			anim.SetBool ("Jumping", true);
 			anim.SetBool ("Falling", false);
-
+			sound.IsJumpingSetter(true);
 		} else if (GetComponent<Rigidbody2D> ().velocity.y < 0) {
 			anim.SetBool ("Jumping", false);
 			anim.SetBool ("Falling", true);
-			falling = true;
-		
 		} else {
 			anim.SetBool ("Jumping", false);
 			anim.SetBool ("Falling", false);
 
 			if (falling)
 			{
-				//particles.GetComponent<Particles>().Emit(5);
-				//falling = false;
+				particles.GetComponent<Particles>().Emit(5);
+				falling = false;
 			}
 		}
-
-
 		
 	}
 	
