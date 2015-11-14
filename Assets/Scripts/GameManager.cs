96ab1ddea0b5bd2b82f3cb5c9e8d0678d1659ce8
Wilky <101wilky@gmail.com>
2015-11-14 03:41:15 +0000

    added length to the platforms length is set on platform creation in game controller

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,29 +6,33 @@ public class GameManager : MonoBehaviour {
 	public GameObject terrain;
 	public float terrainGenCooldown;
 	public Transform[] spawnPoints;
-	public float platformGenCooldown;
+	
 	public float platformCooldown;
 	// Use this for initialization
 	void Start () {
 		terrainGenCooldown = 0.5f;
-		platformGenCooldown = 80.0f;
+		platformCooldown = 80.0f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		terrainGenCooldown--;
 		platformCooldown--;
-		platformGenCooldown--;
+		
 		if (terrainGenCooldown <= 0) {
 			Instantiate (terrain, new Vector3 (11.5f, -4.5f, 0),transform.rotation);
 			terrainGenCooldown = 24f;
 		}
-		if (platformCooldown <= 0 && platformGenCooldown <= 0) {
+		if (platformCooldown <= 0) {
 			int spawnPos = Random.Range(0,4);
 			int spawnLength = Random.Range(2,6);
-			Instantiate (terrain,spawnPoints[spawnPos].position,spawnPoints[spawnPos].rotation);
-			platformCooldown = 80.0f;
-			platformGenCooldown = 80.0f;
+            GameObject platform;
+            platform = Instantiate(terrain, spawnPoints[spawnPos].position, spawnPoints[spawnPos].rotation) as GameObject;
+            platform.AddComponent<platformSpawn>();
+            platform.GetComponent<platformSpawn>().SetSpawnAmmount(spawnLength);
+            platform.GetComponent<platformSpawn>().SetObject(terrain);
+            platformCooldown = 80.0f;
+			
 
 		}
 
---
+++ Assets/Scripts/platformSpawn.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class platformSpawn : MonoBehaviour {
+
+    public GameObject terrain;
+    float spawnCooldown;
+    float offSet = 2.37f;
+    int spawnAmmount;
+
+	// Use this for initialization
+	void Start () {
+        spawnCooldown = 24f;
+       
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        spawnCooldown--;
+	    while (spawnAmmount>0)
+        { 
+            Instantiate(terrain, new Vector3(transform.position.x+offSet,transform.position.y,transform.position.z), transform.rotation);
+            spawnAmmount--;
+            offSet += 2.37f;
+        }
+	}
+
+    public void SetSpawnAmmount(int ammount)
+    {
+        spawnAmmount = ammount;
+        Debug.Log("platform length :"+ ammount);
+    }
+
+    public void SetObject(GameObject obj)
+    {
+        terrain = obj;
+    }
+}
